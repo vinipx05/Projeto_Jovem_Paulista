153941ce0d4c70e48fe943c26d6e616af7ddfa36
Vinicius Gabriel <viniciuspx89@gmail.com>
2026-06-02 16:37:46 -0300

    API conectada ao banco de dados PostgreSQL

--- src/main/java/com/jovempaulista/api/repository/TokenAutenticacaoRepository.java
+++ src/main/java/com/jovempaulista/api/repository/TokenAutenticacaoRepository.java
@@ -1,13 +1,12 @@
 package com.jovempaulista.api.repository;
 
 import com.jovempaulista.api.model.TokenAutenticacao;
-import com.jovempaulista.api.model.VagaHabilidade;
+import com.jovempaulista.api.model.TokenAutenticacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface TokenAutenticacaoRepository  extends JpaRepository<VagaHabilidade,Long> {
-    Optional<TokenAutenticacao> findBytoken(String token);
-    List<TokenAutenticacao> findByusuarioId(long usuarioId);
-
+public interface TokenAutenticacaoRepository extends JpaRepository<TokenAutenticacao, Long> {
+    Optional<TokenAutenticacao> findByToken(String token);
+    List<TokenAutenticacao> findByUsuarioId(Long usuarioId);
 }
--- src/main/java/com/jovempaulista/api/repository/VagaHabilidadeRepository.java
+++ src/main/java/com/jovempaulista/api/repository/VagaHabilidadeRepository.java
@@ -1,12 +1,10 @@
 package com.jovempaulista.api.repository;
 
-import com.jovempaulista.api.model.Habilidade;
 import com.jovempaulista.api.model.VagaHabilidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface VagaHabilidadeRepository  extends JpaRepository<VagaHabilidade,Long> {
-    List<VagaHabilidade> findByVagaId(long vagaId);
-    List<VagaHabilidade> findByObigatoriaId(long obigatoriaId);
-
+public interface VagaHabilidadeRepository extends JpaRepository<VagaHabilidade, Long> {
+    List<VagaHabilidade> findByVagaId(Long vagaId);
+    List<VagaHabilidade> findByObrigatoria(Boolean obrigatoria);
 }
